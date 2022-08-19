e707998a7c83fee5a3a20401743ae3c0c9de490c
Luiz Gouveia <lgou90@gmail.com>
2022-08-19 12:50:12 -0300

    added cart modal =  work in progress

--- script.js
+++ script.js
@@ -196,3 +196,32 @@ function adicionarCarrinho(event) {
     atualizaItens(itens)
     console.log(carrinho)
 }
+
+
+// MODAL CARRINHO
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("modalCarrinhoBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close_carrinho")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
